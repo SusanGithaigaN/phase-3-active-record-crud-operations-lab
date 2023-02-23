2308464f44e3b27d77c525acfe8cc506712e8a06
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-02-23 05:37:39 +0300

    Completed assignment

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -20,4 +20,39 @@ class Movie < ActiveRecord::Base
     def self.movie_count
         self.count
     end
+    # find by id
+    def self.find_movie_with_id(id)
+        find_by(id:id)
+    end
+    
+    def self.find_movie_with_attributes(attributes)
+        where(attributes).first
+    end
+    # find movies after 2002
+    def self.find_movies_after_2002
+        where("release_date > 2002")
+    end
+    # update method
+    def update_with_attributes(attributes)
+        self.update(attributes)
+    end
+
+    def self.update_all_titles(new_title)
+        update_all(title:new_title)
+        new_title
+    end
+    # delete record with corresponding id
+    def self.delete_by_id(id)
+        movie=self.find_by(id:id)
+        if movie
+            movie.delete
+            true
+        else
+            false
+        end
+    end
+    # delete all movies
+    def self.delete_all_movies
+        self.delete_all
+    end
 end
