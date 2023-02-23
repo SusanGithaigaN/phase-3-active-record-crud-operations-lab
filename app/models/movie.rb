0788e6d85fd5537ee8cba085d3936137b72e1a87
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-02-23 04:39:09 +0300

    Testing

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,23 @@
 class Movie < ActiveRecord::Base
 
+    # create a new movie instance with the given title
+    # save the movie to the database
+    # return the movie instance
+    def self.create_with_title(title)
+        movie = self.new(title: title) 
+        movie.save 
+        movie 
+    end
+    # first_movie
+    def self.first_movie
+        self.first
+    end
+    # last_movie
+    def self.last_movie
+        self.last
+    end
+    # movie_count
+    def self.movie_count
+        self.count
+    end
 end
